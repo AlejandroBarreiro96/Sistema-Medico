6cf14323cc3c169378aea06994c3f75fe99a5b85
KevinBarreiro <94024341+KevinBarreiro@users.noreply.github.com>
2023-01-17 23:43:21 -0500

    creacion de plantillas y descarga bootstrap

--- Sistema_medico/Sistema_medico/settings.py
+++ Sistema_medico/Sistema_medico/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SistemaWeb',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Sistema_medico.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r"C:/Users/Usuario\Documents/proyecto/Sistema_medico/SistemaWeb/Template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Sistema_medico/Sistema_medico/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def saludos(request):
-    return HttpResponse("prueba #1 jklsdjkdfsjkdfsjkfsdjksdfjksdf")
