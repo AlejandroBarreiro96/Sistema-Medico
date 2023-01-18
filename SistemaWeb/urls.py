8adc26b3cd6edd40cff890194d142c6565ed4baf
KevinBarreiro <94024341+KevinBarreiro@users.noreply.github.com>
2023-01-17 23:35:23 -0500

    creacion de plantillas y descarga bootstrap

---
+++ SistemaWeb/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from SistemaWeb import views
+urlpatterns = [
+ 
+    path('', views.home, name="Home"),
+    path('servicio', views.servicio, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
+]
