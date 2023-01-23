2192c50bc101b5cf5791cf91cfeaf6065c470bc8
KevinBarreiro <94024341+KevinBarreiro@users.noreply.github.com>
2023-01-22 21:59:30 -0500

    creacion de superusuario

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from  .models import Servicio
 
 # Register your models here.
+
+class servicioadmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+admin.site.register(Servicio, servicioadmin) 
--- settings.py
+++ settings.py
@@ -109,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+
+LANGUAGE_CODE = 'es-ec'
+#LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
