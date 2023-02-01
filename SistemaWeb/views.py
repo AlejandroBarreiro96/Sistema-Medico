97e79e71889c1fd4ad8d18a05df31c00d21f9f56
RoqueCampoverde <94024025+RoqueCampoverde@users.noreply.github.com>
2023-01-31 21:19:16 -0500

    Login

--- SistemaWeb/views.py
+++ SistemaWeb/views.py
@@ -6,11 +6,11 @@ def home(request, template = "SistemaWeb/home.html"):
 def servicio(request, template="SistemaWeb/servicios.html"):
      return render(request, template, {})
 
-def tienda(request, template="SistemaWeb/tienda.html"):
-     return render(request,template,{} )
-
 def blog(request, template ="SistemaWeb/blog.html"):
      return render(request, template, {})
 
 def contacto(request, tempalte="SistemaWeb/contacto.html"):
      return render(request,tempalte,{} )
+
+def login(request, template="SistemaWeb/login.html"):
+     return render(request,template,{} )
